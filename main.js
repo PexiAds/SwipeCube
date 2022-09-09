1879f79c201314cc23f2431cb8389d259b7bc128
NielsTack <niels102@live.nl>
2022-09-09 11:34:23 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+  var rotationCounter = 4;
+  var swiper = new Swiper(".swiper-container", {
+    effect: "cube",
+    grabCursor: true,
+    loop: true,
+    paralax: true,
+    preloadImages: false,
+    lazy: false,
+    autoplay: true,
+    speed: 500,
+    cubeEffect: {
+      shadow: false,
+      slideShadows: true,
+      shadowOffset: 10,
+      shadowScale: 0.94
+    },
+  });
+var slidecounter = 0
+  swiper.on("slideChange", function() {
+    --rotationCounter;
+    slidecounter = swiper.activeIndex;
+    if (rotationCounter <= 0) {
+      swiper.autoplay.stop();
+    }
+  });
+  $('#stage').on('click',function(){
+    PEXI.event('click_'+slidecounter)
+    PEXI.click();
+  })
+
+  swiper.on("touchStart", function() {
+    $(".swipe-hand").fadeOut();
+    PEXI.event('user_move')
+  });
+
+TweenMax.to('.hiddenOnload',.3,{opacity:1})
